20d8e7385e2a1f6daf2ead0e1306371dd880cfe1
Lizzie Sorin <lizzie.sorin@hotmail.fr>
2017-04-21 15:17:33 +0000

    Done.

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,7 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+# Create your CostumeStore class here
+
+class Costume < ActiveRecord::Base
+end
--- app/models/costume_store.rb
+++ app/models/costume_store.rb
@@ -1,1 +1,7 @@
 # Create your CostumeStore class here
+
+# class CostumeStore < ActiveRecord::Base
+# end
+
+class CostumeStore < ActiveRecord::Base
+end
